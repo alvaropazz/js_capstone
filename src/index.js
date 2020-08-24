3e87c7062def497acffd616c880c92c38d00c813
A L V A R O <alvarop3000@gmail.com>
2020-08-24 18:02:54 -0500

    add movement to sprite

--- src/index.js
+++ src/index.js
@@ -7,39 +7,78 @@ function preload() {
   this.load.image('obstacles', './assets/objects.png')
   this.load.image('decorations', './assets/decoration.png')
   this.load.tilemapTiledJSON('map', './assets/world.json')
+  this.load.spritesheet('person', './assets/character.png', { frameWidth: 16, frameHeight: 32 })
 }
 
 let controls;
+let player;
+let cursors;
 
 function create() {
   const map = this.make.tilemap({ key: 'map' });
   const terrainTiles = map.addTilesetImage('Overworld', 'world');
   const obstacleTiles = map.addTilesetImage('objects', 'obstacles');
-  const decorTiles = map.addTilesetImage('decoration', 'decorations')
+  const decorTiles = map.addTilesetImage('decoration', 'decorations');
+
+  player = this.physics.add.sprite(120,400, 'person')
+  player.setDepth(1);
+
 
   const mainLayer = map.createStaticLayer('terrain', terrainTiles, 0, 0);
   const secondLayer = map.createStaticLayer('walls', obstacleTiles, 0, 0);
   const thirdLayer = map.createStaticLayer('decoration', decorTiles, 0, 0);
 
-  const camera = this.cameras.main;
+  secondLayer.setCollisionByProperty({ collides: true });
+  thirdLayer.setCollisionByProperty({ collides: true });
 
-  // Set up the arrows to control the camera
-  const cursors = this.input.keyboard.createCursorKeys();
-  controls = new Phaser.Cameras.Controls.FixedKeyControl({
-    camera: camera,
-    left: cursors.left,
-    right: cursors.right,
-    up: cursors.up,
-    down: cursors.down,
-    speed: 0.5
-  });
+  const camera = this.cameras.main;
+  camera.startFollow(player);
   camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+
+  cursors = this.input.keyboard.createCursorKeys();
+
+  // const camera = this.cameras.main;
+
+  // // Set up the arrows to control the camera
+  
+  // const cursors = this.input.keyboard.createCursorKeys();
+  
+  // controls = new Phaser.Cameras.Controls.FixedKeyControl({
+  //   camera: camera,
+  //   left: cursors.left,
+  //   right: cursors.right,
+  //   up: cursors.up,
+  //   down: cursors.down,
+  //   speed: 0.5
+  // });
+  // camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
 
 }
 
 function update(time, delta) {
+  const speed = 175;
+
   // Apply the controls to the camera each update tick of the game
-  controls.update(delta);
+   // Stop any previous movement from the last frame
+   player.body.setVelocity(0);
+
+   // Horizontal movement
+   if (cursors.left.isDown) {
+     player.body.setVelocityX(-speed);
+   } else if (cursors.right.isDown) {
+     player.body.setVelocityX(speed);
+   }
+ 
+   // Vertical movement
+   if (cursors.up.isDown) {
+     player.body.setVelocityY(-speed);
+   } else if (cursors.down.isDown) {
+     player.body.setVelocityY(speed);
+   }
+ 
+   // Normalize and scale the velocity so that player can't move faster along a diagonal
+   player.body.velocity.normalize().scale(speed);
+  // controls.update(delta);
 }
 
 const config = {
@@ -51,6 +90,13 @@ const config = {
     preload: preload,
     create: create,
     update: update
+  },
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 0 },
+      // enableBody: true
+    }
   }
 };
 
