0c5af332c8b6c3b34b364c4a0af33de178e5cebb
A L V A R O <alvarop3000@gmail.com>
2020-08-28 19:51:45 -0500

    fix model file

--- src/objects/model.js
+++ src/objects/model.js
@@ -2,7 +2,6 @@
 
 export default class Model {
   constructor() {
-    this._soundOn = true;
     this._musicOn = true;
     this._bgMusicPlaying = false;
   }
@@ -15,27 +14,11 @@ export default class Model {
     return this._musicOn;
   }
 
-  set soundOn(value) {
-    this._soundOn = value;
-  }
-
-  get soundOn() {
-    return this._soundOn;
-  }
-
   set bgMusicPlaying(value) {
     this._bgMusicPlaying = value;
   }
 
   get bgMusicPlaying() {
     return this._bgMusicPlaying;
-  }
-
-  set name(value) {
-    this._user = value;
-  }
-
-  get name() {
-    return this._user;
   }
 }
